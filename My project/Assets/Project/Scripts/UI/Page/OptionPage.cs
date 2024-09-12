ff92f81ac7e94daf117cdeb60d358da0fc3cecc7
kimyir <kimyir2@gmail.com>
2024-09-12 21:01:54 +0900

    갈무리 폰트 추가

--- My project/Assets/Project/Scripts/UI/Page/OptionPage.cs
+++ My project/Assets/Project/Scripts/UI/Page/OptionPage.cs
@@ -7,6 +7,16 @@ namespace UI.Page
     [Binding]
     public class OptionPage : PageViewModel
     {
-        
+        [Binding]
+        public void OnClickKorean()
+        {
+            I2.Loc.LocalizationManager.CurrentLanguage = "English (United States)";  //Language and Variant
+        }
+        [Binding]
+        public void OnClickEnglish()
+        {
+            I2.Loc.LocalizationManager.CurrentLanguage = "English (United States)";  //Language and Variant
+            Global.UIManager.ClosePage();
+        }
     }
 }
