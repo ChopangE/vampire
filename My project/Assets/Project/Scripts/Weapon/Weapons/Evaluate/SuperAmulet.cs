5285ed9ed9e1113a6a681c1d65f7f485a5f1682d
kimyir <kimyir2@gmail.com>
2025-01-30 14:22:48 +0900

    강화 폭발 부적도 랜덤방향으로 발사되게 구현

--- My project/Assets/Project/Scripts/Weapon/Weapons/Evaluate/SuperAmulet.cs
+++ My project/Assets/Project/Scripts/Weapon/Weapons/Evaluate/SuperAmulet.cs
@@ -10,8 +10,13 @@ public class SuperAmulet : Weapon
     }
     public override void ExecuteAttack()
     {
+        // 랜덤한 각도 생성 (0~360도)
+        float randomAngle = Random.Range(0f, 360f);
+        Vector3 dir = Quaternion.Euler(0, 0, randomAngle) * Vector3.up;
+
         Transform bullet = GameManager.Instance.pool.Get(prefabId).transform;
         bullet.position = transform.position;
-        bullet.GetChild(0).GetComponent<Bullet>().Init(damage, -1, Vector3.zero, criticalChancePercent:criticalChancePercent, criticalDamagePercent:criticalDamagePercent);
+        bullet.GetChild(0).GetComponent<Bullet>().Init(damage, -1, dir, 
+        criticalChancePercent:criticalChancePercent, criticalDamagePercent:criticalDamagePercent);
     }
 }
